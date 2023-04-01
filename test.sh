9a1da8e05a82c7b990b841d4f26723be08721687
yizhou.chen <yizhou.chen@grabtaxi.com>
2023-04-01 12:08:49 +0800

    f action

--- test.sh
+++ test.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+COMMIT_MESSAGE="Automatic Generate Timestamp"
+PREV_COMMIT=$(git rev-parse HEAD^)
+PREV_COMMIT_MESSAGE=$(git log --format=%B -n 1 $PREV_COMMIT)
+
+if [[ "$PREV_COMMIT_MESSAGE" == COMMIT_MESSAGE ]]; then
+    return
+fi
+
+
 # 创建文件并写入时间戳
 echo $(date +%s) >> timestamp.txt
 git config user.email "15120073241@126.com"
 git config user.name "Yizhou"
 git add timestamp.txt
-git commit -m "Modify example.txt"
+git commit -m COMMIT_MESSAGE
 git push origin main
